3540733012fcd143222e5fda2a6a6440ebfb92c1
CMKyalo <166713235+CMKyalo@users.noreply.github.com>
2025-09-28 20:18:58 +0300

    ppt 2

--- login.php
+++ login.php
@@ -27,6 +27,7 @@ session_start();
             if (password_verify($password, $user['password'])) {
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['username'] = $user['username'];
+                
                 header("Location: index.php");
                 exit;
             } else {
